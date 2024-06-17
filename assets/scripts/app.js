68bc46b7d4e5bdadb1f3cc130d722b91c156d173
TASSOS PSIMITIS <127046265+Tassps@users.noreply.github.com>
2024-06-17 21:02:20 +0000

    "I made an import obejct in the app.js file where a group all the exported variables from the util.js file"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 // import { apiKey } from "./util.js";
 
 // import apiKey from "./util.js";
-import { apiKey, abc } from "./util.js";
+// import { apiKey, abc } from "./util.js";
+import * as util from "./util.js";
 
-console.log(apiKey);
+console.log(util.apiKey);
