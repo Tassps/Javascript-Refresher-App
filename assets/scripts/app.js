32ed045e3e3887cf3cb9698884fbbcce0769c7a1
TASSOS PSIMITIS <127046265+Tassps@users.noreply.github.com>
2024-06-19 19:52:34 +0000

    "I commented the code in the app.js file"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
 // import { apiKey } from "./util.js";
 
 // import apiKey from "./util.js";
-import { apiKey, abc as content } from "./util.js";
+// import { apiKey, abc as content } from "./util.js";
 // import * as util from "./util.js";
 
-console.log(content);
+// console.log(util.apiKey);
+// console.log(content);
