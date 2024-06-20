cf554bbfeca0434ce4133cfbb60a92104d20e66f
TASSOS PSIMITIS <127046265+Tassps@users.noreply.github.com>
2024-06-20 10:52:53 +0000

    "I tested the operators of +, /, -, *."

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,3 +11,8 @@
 
 // console.log(userMessage);
 // console.log(userMessage);
+
+console.log(10 + 5); // I used the + operator to add values
+console.log(10 / 5); // I useed the / operator to devide values
+console.log(10 - 5); // I used the - operator to subtract values
+console.log(10 * 5); // I used the * operator to multiply values
