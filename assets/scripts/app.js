323fbdbd4fe7bc557a9ad2820a51c75b7f01b8e4
TASSOS PSIMITIS <127046265+Tassps@users.noreply.github.com>
2024-06-21 08:32:37 +0000

    "I commented out the code at the app.js file"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,15 +12,15 @@
 // console.log(userMessage);
 // console.log(userMessage);
 
-console.log(10 + 5); // I used the + operator to add values
-console.log(10 / 5); // I useed the / operator to devide values
-console.log(10 - 5); // I used the - operator to subtract values
-console.log(10 * 5); // I used the * operator to multiply values
+// console.log(10 + 5); // I used the + operator to add values
+// console.log(10 / 5); // I useed the / operator to devide values
+// console.log(10 - 5); // I used the - operator to subtract values
+// console.log(10 * 5); // I used the * operator to multiply values
 
-console.log("Hello" + " world"); // I used the + operator to concatenate the two strings
-console.log(10 === 5); // I used the === sign to check for equality
-console.log(10 === 10); // I used the === sign to check for equality
-console.log(10 < 10); // I used the <, > operators to check if the values are smaller or greater
-console.log(10 > 10);
-console.log(10 <= 10); // I used the <=, => operators to check if the values are greater or equal or smaller or equal
-console.log(10 >= 10);
+// console.log("Hello" + " world"); // I used the + operator to concatenate the two strings
+// console.log(10 === 5); // I used the === sign to check for equality
+// console.log(10 === 10); // I used the === sign to check for equality
+// console.log(10 < 10); // I used the <, > operators to check if the values are smaller or greater
+// console.log(10 > 10);
+// console.log(10 <= 10); // I used the <=, => operators to check if the values are greater or equal or smaller or equal
+// console.log(10 >= 10);
