b9677bf07e699085a888e1ce030b029cee236371
TASSOS PSIMITIS <127046265+Tassps@users.noreply.github.com>
2024-06-22 09:34:14 +0000

    "I commented out the code in the app.js file"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -40,19 +40,19 @@
 // }
 
 // This function creates a greeting string now for a user
-function createGreeting(userName, message = "Hello!") {
-  // console.log(userName);
-  // console.log(message);
-  return "Hi, I am " + userName + ". " + message;
-}
+// function createGreeting(userName, message = "Hello!") {
+//   // console.log(userName);
+//   // console.log(message);
+//   return "Hi, I am " + userName + ". " + message;
+// }
 
 // greet(); // Calling or invoking the function greet
 
 // greetUser("Kostas"); // I called the function greetUser and placed 2 input values in it
 // greetUser("George", "Hello, what's up?"); // I called the function greetUser and placed a new set of input values in it
 
-const greeting1 = createGreeting("Kostas"); // I stored the fuction return value in a constant variable for a readability perspective
-console.log(greeting1);
+// const greeting1 = createGreeting("Kostas"); // I stored the fuction return value in a constant variable for a readability perspective
+// console.log(greeting1);
 
-const greeting2 = createGreeting("George", "Hello, what's up?"); // I stored the function return value in a constant variable for a readability perspective
-console.log(greeting2);
+// const greeting2 = createGreeting("George", "Hello, what's up?"); // I stored the function return value in a constant variable for a readability perspective
+// console.log(greeting2);
