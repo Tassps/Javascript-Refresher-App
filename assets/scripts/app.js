50cdf510410c2eea670474ebf141e3899fb93129
Tassos Psimitis <tassospsimitis@gmail.com>
2024-07-18 23:02:34 +0300

    " I created a constant of an array called numbers containing 5 elements. "

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -479,4 +479,6 @@
 // Expected output: false, because 60 is not an element of numbers.
 
 // some() method of Array instances tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. It doesn't modify the array.
-// For example: 
+// For example:
+
+const numbers = [1, 2, 3, 4, 5]; 
