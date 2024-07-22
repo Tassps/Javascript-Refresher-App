bfc091839894ea6ea047c121a38c3a5457a0d170
Tassos Psimitis <tassospsimitis@gmail.com>
2024-07-22 22:36:59 +0300

    " I declare a constant called array2 and assign to it an array with 5 elements. "

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -503,4 +503,6 @@
 // reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of runnning the reducer across all elements of the array is a single value.
 // The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place. Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
 
+const array2 = [2, 3, 4, 5];
+
 
