c6967e012d5bfd34fd134a69054c32634598e43b
Tassos Psimitis <tassospsimitis@gmail.com>
2024-07-25 22:25:38 +0300

    " I declared a constant array named fruits which contains 5 elements. "

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -531,3 +531,7 @@
 
 // slice() method of Array instances returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
 // For example:
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+
