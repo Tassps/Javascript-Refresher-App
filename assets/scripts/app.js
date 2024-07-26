fbb4d23a9b7d042158ece38886d4c1ea3a2e46c7
Tassos Psimitis <tassospsimitis@gmail.com>
2024-07-26 22:51:18 +0300

    " I commented on the code, in the line 552 and explained it in detail. "

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -549,4 +549,5 @@ console.log(fruits.slice(-3)); // I used slice on this line which returns a new 
 console.log(fruits.slice(2, -1)); // I used slice on this line which returns a new array which starts at index 2 and go up to, but mot including, the second-to-last element.
 // Expected output: New Array ["cherry", "date"]
 
-console.log(fruits.slice());
+console.log(fruits.slice()); // I used slice on this line with no arguments, which creates a shallow copy of the entire array.
+// Expected output: New Array ["apple", "banana", "cherry", "date", "pineapple"]
